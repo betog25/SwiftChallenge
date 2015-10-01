3d52278c2283107e619795842c13c89ebd5bea34
betog25 <beth.goldberg1@gmail.com>
2015-09-30 19:15:23 -0700

    Update Challenge.java

--- Challenge.java
+++ Challenge.java
@@ -1,14 +1,12 @@
 public class Challenge
 {
 	/**
+	 * fizzBuzz
 	 * 
-	 * 
-	 * 
-	 * 
-	 * 
+	 * This function counts from 0 to 100 and calls printResult() to 
+	 * print each number in succession. 
 	 * 
 	*/
-	 
 	public void fizzBuzz()
 	{
 		for(int i = 0; i<= 100; i++)
@@ -17,6 +15,17 @@ public class Challenge
 		}
 	}
 	
+	/**
+ 	* printResult
+ 	* 
+ 	* Function that accepts an integer and returns "Fizz" every 
+ 	* time the integer is a multiple of 3, returns "Buzz"
+	* every time the integer is a multiple of 5, and returns 
+	* the number otherwise.
+ 	*
+ 	* @param num The integer being evaluated
+ 	* @return string to print
+ 	*/
 	public String printResult( int num )
 	{
 		if( num %3 == 0 && num %5 == 0 && num != 0)
